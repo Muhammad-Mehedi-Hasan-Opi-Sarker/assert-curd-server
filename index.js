d37d7b9839d5ec9f40a216b63e44f1959c69dfbd
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-01 17:50:31 +0600

    add client site form and data sent for mongodb

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// user name: assertData
+// password: FXsI6o1dqfRikjXm
+
+const uri = "mongodb+srv://assertData:FXsI6o1dqfRikjXm@cluster0.ijybm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const dataCollection = client.db('data').collection('user');
+        app.post('/user', async (req, res) => {
+            const list = req.body;
+            const result = await dataCollection.insertOne(list);
+            res.send(result);
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('curd operation is running')
+    res.send('curd operation is running')
 })
 
 app.listen(port, () => {
-  console.log('connected')
+    console.log('connected')
 })
