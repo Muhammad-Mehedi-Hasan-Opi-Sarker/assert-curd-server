c1973979bb0a0d76be494c6b7fe9ef40facfa3ad
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-02 12:47:47 +0600

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, AuthMechanism } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const { ObjectID } = require('bson');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ app.use(express.json());
 // user name: assertData
 // password: FXsI6o1dqfRikjXm
 
-const uri = "mongodb+srv://assertData:FXsI6o1dqfRikjXm@cluster0.ijybm.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijybm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
